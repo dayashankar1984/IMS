b66e48312157a311f9c1c480604700860633f6ed
dayashankar1984 <142090450+dayashankar1984@users.noreply.github.com>
2023-11-04 08:04:36 +0530

    supplier.py

--- supplier.py
+++ supplier.py
@@ -8,7 +8,7 @@ class supplierclass:
         self.root=root
         self.root.geometry("1100x600+220+130")
         self.root.title("Inventory Managment System | Developed By Daya")
-        self.root.config(bg="white",)
+        self.root.config(bg="white")
         #root.resizable(False,False)
         self.root.focus_force()
         
@@ -21,53 +21,46 @@ class supplierclass:
         self.var_name=StringVar()
         self.var_contact=StringVar()
         
-
-        #==== Searchfram ===
-        searchfram=LabelFrame(self.root,text="Search Employee",font=("goudy old style,",12,"bold"),bd=2,relief=RIDGE,bg="white")
-        searchfram.place(x=250,y=20,width=600,height=70)
-        
+        #==== Searchfram ===       
         #==== Options =======
-        cmb_search=ttk.Combobox(searchfram,textvariable=self.var_searchBy,values=("Select","Email","Name","Contact"),state='readonly',justify=CENTER,font=("gudy old style",15))
-        cmb_search.place(x=10,y=10,width=180)
-        cmb_search.current(0)
+        lbl_search=Label(self.root,text="Invoice No.",bg="white",font=("gudy old style",15))
+        lbl_search.place(x=700,y=80)
 
-        txt_search=Entry(searchfram,textvariable=self.var_searchtxt,font=("goudy old style",15),bg="light yellow").place(x=200,y=12)
-        btn_search=Button(searchfram,text="Search",command=self.search,font=("goudy old style",15,"bold"),bg="#4caf50",fg="white",cursor="hand2").place(x=410,y=9,width=150,height=30)
+        txt_search=Entry(self.root,textvariable=self.var_searchtxt,font=("goudy old style",15),bg="light yellow").place(x=800,y=80,width=143,height=30)
+        btn_search=Button(self.root,text="Search",command=self.search,font=("goudy old style",15,"bold"),bg="#4caf50",fg="white",cursor="hand2").place(x=950,y=79,width=100,height=30)
 
         #===== Tital =======
-        tit=Label(self.root,text="Supplier Details",font=("goudy old style",15,"bold",),bg="#0f4d7d",fg="white").place(x=50,y=100,width=1000)
-
+        tit=Label(self.root,text="Supplier Details",font=("goudy old style",20,"bold",),bg="#0f4d7d",fg="white").place(x=50,y=10,width=1000,height=40)
 
         #======== Content ========
         #====== Row1 ========
 
-        lal_supplier_invoice=Label(self.root,text="Invoice No",font=("goudy old style",15,"bold",),bg="white").place(x=50,y=150,)   
-        txt_supplier_invoice=Entry(self.root,textvariable=self.var_sup_invoice,font=("goudy old style",15,"bold",),bg="Lightyellow").place(x=150,y=150,width=180)
+        lal_supplier_invoice=Label(self.root,text="Invoice No",font=("goudy old style",15,"bold",),bg="white").place(x=50,y=80,)   
+        txt_supplier_invoice=Entry(self.root,textvariable=self.var_sup_invoice,font=("goudy old style",15,"bold",),bg="Lightyellow").place(x=180,y=80,width=180)
     
-
         #===== Row2 ======
-        lal_name=Label(self.root,text="Supplier Name",font=("goudy old style",15,"bold",),bg="white").place(x=50,y=190,)
-        txt_name=Entry(self.root,textvariable=self.var_name,font=("goudy old style",15,"bold",),bg="Lightyellow").place(x=150,y=190,width=180)
+        lal_name=Label(self.root,text="Name",font=("goudy old style",15,"bold",),bg="white").place(x=50,y=120,)
+        txt_name=Entry(self.root,textvariable=self.var_name,font=("goudy old style",15,"bold",),bg="Lightyellow").place(x=180,y=120,width=180)
         
         #===== Row3 ======
-        lal_contact=Label(self.root,text="Contact",font=("goudy old style",15,"bold",),bg="white").place(x=50,y=230,)
-        txt_contact=Entry(self.root,textvariable=self.var_contact,font=("goudy old style",15,"bold",),bg="Lightyellow").place(x=150,y=230,width=180)
+        lal_contact=Label(self.root,text="Contact",font=("goudy old style",15,"bold",),bg="white").place(x=50,y=160,)
+        txt_contact=Entry(self.root,textvariable=self.var_contact,font=("goudy old style",15,"bold",),bg="Lightyellow").place(x=180,y=160,width=180)
         
         #===== Row4 ======
-        lal_desc=Label(self.root,text="Description",font=("goudy old style",15,"bold",),bg="white").place(x=50,y=270,)
+        lal_desc=Label(self.root,text="Description",font=("goudy old style",15,"bold",),bg="white").place(x=50,y=200,)
         self.txt_desc=Text(self.root,font=("goudy old style",15,"bold",),bg="Lightyellow")
-        self.txt_desc.place(x=150,y=270,width=300,height=60)
+        self.txt_desc.place(x=180,y=200,width=470,height=120)
 
         #====== buttons =======
-        btn_add=Button(self.root,text="Save",command=self.add,font=("goudy old style",15,"bold"),bg="#2196f3",fg="white",cursor="hand2").place(x=500,y=305,width=110,height=28)
-        btn_update=Button(self.root,text="Update",command=self.update,font=("goudy old style",15,"bold"),bg="#4caf50",fg="white",cursor="hand2").place(x=620,y=305,width=110,height=28)
-        btn_delete=Button(self.root,text="Delete",command=self.Delete,font=("goudy old style",15,"bold"),bg="#f44336",fg="white",cursor="hand2").place(x=740,y=305,width=110,height=28)
-        btn_clear=Button(self.root,text="Clear",command=self.clear,font=("goudy old style",15,"bold"),bg="#607d8b",fg="white",cursor="hand2").place(x=860,y=305,width=110,height=28)
+        btn_add=Button(self.root,text="Save",command=self.add,font=("goudy old style",15,"bold"),bg="#2196f3",fg="white",cursor="hand2").place(x=180,y=370,width=110,height=35)
+        btn_update=Button(self.root,text="Update",command=self.update,font=("goudy old style",15,"bold"),bg="#4caf50",fg="white",cursor="hand2").place(x=300,y=370,width=110,height=35)
+        btn_delete=Button(self.root,text="Delete",command=self.Delete,font=("goudy old style",15,"bold"),bg="#f44336",fg="white",cursor="hand2").place(x=420,y=370,width=110,height=35)
+        btn_clear=Button(self.root,text="Clear",command=self.clear,font=("goudy old style",15,"bold"),bg="#607d8b",fg="white",cursor="hand2").place(x=540,y=370,width=110,height=35)
 
-        #====== Emplyee Details ===========
+        #====== Supplier Details ===========
 
-        emp_frame=Frame(self.root,bd=3,relief=RIDGE)
-        emp_frame.place(x=0,y=350,relwidth=1,height=250) # fram Size
+        emp_frame=Frame(self.root,bd=3,relief=RIDGE,bg="blue")
+        emp_frame.place(x=700,y=150,width=350,height=350) # fram Size
 
         Scrolly=Scrollbar(emp_frame,orient=VERTICAL)
         Scrollx=Scrollbar(emp_frame,orient=HORIZONTAL)
@@ -91,7 +84,7 @@ class supplierclass:
         self.suppliertable.column("desc",width=100)
         self.suppliertable.pack(fill=BOTH,expand=1)
         self.suppliertable.bind("<ButtonRelease-1>",self.get_data)
-        #self.show()
+        self.show()
 #======================================================================================================================
 
     def add(self):
@@ -116,14 +109,13 @@ class supplierclass:
                     messagebox.showinfo("Success","supplier Addedd Successfully",parent=self.root)
                     self.show()
         except Exception as ex:
-            messagebox.showerror("Error",f"Error due to : {str(ex)}",parent=self.root)
-                                                                                           
+            messagebox.showerror("Error",f"Error due to : {str(ex)}",parent=self.root)                                                                              
     
     def show(self):
         con=sqlite3.connect(database=r'ims.db')
         cur=con.cursor()
         try:
-            cur.execute("Select * from employee")
+            cur.execute("Select * from supplier")
             rows=cur.fetchall()
             self.suppliertable.delete(*self.suppliertable.get_children())
             for row in rows:
@@ -141,47 +133,32 @@ class supplierclass:
         #print(row)
         self.var_sup_invoice.set(row[0])
         self.var_name.set(row[1])
-        self.var_email.set(row[2])
-        self.var_gender.set(row[3])
-        self.var_contact.set(row[4])
-        self.var_dob.set(row[5])
-        self.var_doj.set(row[6])
-
-        self.var_pass.set(row[7])
-        self.var_utype.set(row[8])
+        self.var_contact.set(row[2])
         self.txt_desc.delete('1.0',END),
-        self.txt_desc.insert(END,row[9]),
-        self.var_salary.set(row[10])
+        self.txt_desc.insert(END,row[3]),
 
     def update(self):
         con=sqlite3.connect(database=r'ims.db')
         cur=con.cursor()
         try:
             if self.var_sup_invoice.get()=="":
-               messagebox.showerror("Error","Employee ID Must be Required",parent=self.root)
+               messagebox.showerror("Error","Invoice no. Must be Required",parent=self.root)
             else:
-                cur.execute("Select * from employee where eid=?",(self.var_sup_invoice.get(),))
+                cur.execute("Select * from supplier where invoice=?",(self.var_sup_invoice.get(),))
                 row=cur.fetchone()
                 if row==None:
-                    messagebox.showerror("Error","invalid Employee ID",parent=self.root)
+                    messagebox.showerror("Error","invalid Invoice No",parent=self.root)
                 else:
-                    cur.execute("Update employee set name=?,email=?,gender=?,contact=?,dob=?,doj=?,pass=?,utype=?,address=?,salary=? where eid=?",(
+                    cur.execute("Update supplier set name=?,contact=?,desc=? where invoice=?",(
                                                 
                                                 self.var_name.get(),
-                                                self.var_email.get(),
-                                                self.var_gender.get(),
                                                 self.var_contact.get(),
-                                                self.var_dob.get(),
-                                                self.var_doj.get(),
-                                                self.var_pass.get(),
-                                                self.var_utype.get(),
                                                 self.txt_desc.get('1.0',END),
-                                                self.var_salary.get(),
                                                 self.var_sup_invoice.get(),
 
                     ))
                     con.commit()
-                    messagebox.showinfo("Success","Employee Updated Successfully",parent=self.root)
+                    messagebox.showinfo("Success","Supplier Updated Successfully",parent=self.root)
                     self.show()
         except Exception as ex:
             messagebox.showerror("Error",f"Error due to : {str(ex)}",parent=self.root)
@@ -191,19 +168,19 @@ class supplierclass:
         cur=con.cursor()
         try:
             if self.var_sup_invoice.get()=="":
-               messagebox.showerror("Error","Employee ID Must be Required",parent=self.root)
+               messagebox.showerror("Error","Invoice no. must be Required",parent=self.root)
             else:
-                cur.execute("Select * from employee where eid=?",(self.var_sup_invoice.get(),))
+                cur.execute("Select * from supplier where invoice=?",(self.var_sup_invoice.get(),))
                 row=cur.fetchone()
                 if row==None:
-                    messagebox.showerror("Error","invalid Employee ID",parent=self.root)
+                    messagebox.showerror("Error","invalid Invoice No",parent=self.root)
                 else:
                     op=messagebox.askyesno("Confirm","Do You really want to delete?",parent=self.root)
                     if op==True:
-                        cur.execute("Delete from employee where eid=?",(self.var_sup_invoice.get(),))
+                        cur.execute("Delete from supplier where invoice=?",(self.var_sup_invoice.get(),))
                         con.commit()
                         self.show()
-                        messagebox.showinfo("Delete","Employee Deleted Successfully",parent=self.root)
+                        messagebox.showinfo("Delete","Supplier Deleted Successfully",parent=self.root)
                         self.clear()
         except Exception as ex:
             messagebox.showerror("Error",f"Error due to : {str(ex)}",parent=self.root)
@@ -211,37 +188,24 @@ class supplierclass:
     def clear(self):
         self.var_sup_invoice.set("")
         self.var_name.set("")
-        self.var_email.set("")
-        self.var_gender.set("Select")
         self.var_contact.set("")
-
-        self.var_dob.set("")
-        self.var_doj.set("")
-
-        self.var_pass.set("")
-        self.var_utype.set("Admin")
         self.txt_desc.delete('1.0',END),
-        self.var_salary.set("")
         self.var_searchtxt.set("")
-        self.var_searchBy.set("Select")
         self.show()
 
     def search(self):
         con=sqlite3.connect(database=r'ims.db')
         cur=con.cursor()
         try:
-            if self.var_searchBy.get()=="Select":
-                messagebox.showerror("Error","Select search By option",parent=self.root)
-            elif self.var_searchtxt.get()=="":
-                messagebox.showerror("Error","Search input should be required",parent=self.root)
+            if self.var_searchtxt.get()=="":
+                messagebox.showerror("Error","Invoice No. should be required",parent=self.root)
 
             else:
-                cur.execute("select * from employee where "+self.var_searchBy.get()+" LIKE '%"+self.var_searchtxt.get()+"%'")
-                rows=cur.fetchall()
-                if len(rows)!=0:
+                cur.execute("select * from supplier where invoice=?",(self.var_searchtxt.get(),))
+                row=cur.fetchone()
+                if row!=None:
                     self.suppliertable.delete(*self.suppliertable.get_children())
-                    for row in rows:
-                        self.suppliertable.insert('',END,values=row)
+                    self.suppliertable.insert('',END,values=row)
                 else:
                     messagebox.showerror("Error","No record found !!!",parent=self.root)
         except Exception as ex:

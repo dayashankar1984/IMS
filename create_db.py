52e036641832ab9d8affbb2b623d0ca9684d6ca1
dayashankar1984 <142090450+dayashankar1984@users.noreply.github.com>
2023-11-04 18:00:45 +0530

    create_db.py

--- create_db.py
+++ create_db.py
@@ -8,4 +8,7 @@ def create_db():
     cur.execute("CREATE TABLE IF NOT EXISTS supplier(invoice INTEGER PRIMARY KEY AUTOINCREMENT,name text,contact text,desc text)") 
     con.commit()
 
+    cur.execute("CREATE TABLE IF NOT EXISTS category(cid INTEGER PRIMARY KEY AUTOINCREMENT,name text)") 
+    con.commit()
+
 create_db()
